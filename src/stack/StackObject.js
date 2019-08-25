cb788ca34dcb4acf6752ac3755c19029d117884a
haitaodesign <15393243613@163.com>
2019-08-25 15:49:57 +0800

    feat: 基于对象的 Stack 类

---
+++ src/stack/StackObject.js
@@ -0,0 +1,52 @@
+class StackObject {
+  constructor () {
+    this.count = 0
+    this.items = {}
+  }
+  push (element) {
+    this.items[this.count] = element
+    this.count++
+  }
+  pop () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    this.count--
+    const result = this.items[this.count]
+    delete this.items[this.count]
+    return result
+  }
+  peek () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    return this.items[this.count - 1]
+  }
+  isEmpty () {
+    return this.count === 0
+  }
+  clear () {
+    // 这里没有遵循后进先出原则
+    this.items = []
+    this.count = 0
+    // 使用下面的实现遵循后进先出原则
+    // while (!this.isEmpty()) {
+    //   this.pop()
+    // }
+  }
+  size () {
+    return this.count
+  }
+  toString () {
+    if (this.isEmpty()) {
+      return ''
+    }
+    let objString = `${this.items[0]}`
+    for (let i = 0; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`
+    }
+    return objString
+  }
+}
+
+export default StackObject
--- src/stack/app.js
+++ src/stack/app.js
@@ -1,5 +1,7 @@
 import StackArray from './StackArray'
+import StackObject from './StackObject'
 
+//  基于数组的 Stack 类
 let stackArray = new StackArray()
 
 console.log(stackArray)
@@ -23,3 +25,27 @@ stackArray.pop()
 stackArray.pop()
 
 console.log(stackArray.size())
+
+// 基于对象的 Stack 类
+
+let stackObject = new StackObject()
+
+console.log(stackObject)
+
+stackObject.push(5)
+stackObject.push(8)
+
+stackObject.push(11)
+
+console.log(stackObject.size())
+
+console.log(stackObject.peek())
+
+console.log(stackObject.isEmpty())
+
+stackObject.push(15)
+
+stackObject.pop()
+stackObject.pop()
+
+console.log(stackObject.size())
