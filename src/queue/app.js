6346a70d03c560d518ae4de141a35661046da64b
haitaodesign <15393243613@163.com>
2019-08-27 22:24:50 +0800

    feat: 新增 Queue 使用example

--- src/queue/app.js
+++ src/queue/app.js
@@ -1,1 +1,20 @@
-console.log('test')
+import Queue from './Queue'
+
+const queue = new Queue()
+
+console.log(queue)
+
+console.log(queue.isEmpty()) // true
+
+queue.enqueue('li')
+queue.enqueue('hai')
+queue.enqueue('tao')
+
+console.log(queue.toString())
+
+console.log(queue.isEmpty())
+
+console.log(queue.size())
+
+queue.dequeue('li')
+console.log(queue.toString())
